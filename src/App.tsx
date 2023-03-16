b7e9ba505b188be2a72a8ab53211c8267e49384a
Adam Ciecko <adam.ciecko@hotmail.com>
2023-03-16 22:50:17 +0100

    skeletal of store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.scss";
+import { useAppDispatch } from "./store";
+import { useAppSelector } from "./store/useSelector";
+import {authenticate, deauthenticate} from "./store/AuthSlice/AuthSlice";
+
 
 function App() {
+  const isAuth = useAppSelector((s) => s.auth.isAuthenticated);
+  const dispatch = useAppDispatch()
+
   return (
+    <>
+    {isAuth ? "Yeag": "Nope"}
       <div>Works ?</div>
+      <button onClick={()=> dispatch(authenticate())}>Auth</button>
+      <button onClick={()=> dispatch(deauthenticate())}>Deauth</button>
+    </>
   );
 }
 
--- src/store/AuthSlice/AuthSlice.ts
+++ src/store/AuthSlice/AuthSlice.ts
@@ -1,17 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState: {
-      isAuthenticated:false,
-    },
-    reducers: {
-        login: () => {
+  name: "auth",
+  initialState: {
+    isAuthenticated: false,
+  },
+  reducers: {
+    login: () => ({
+       isAuthenticated: true,
+    }),
+    logout: () => ({
+      isAuthenticated: false,
+    }),
+  },
+});
 
-        },
-        logout: () => {
-
-        }
-    },
-  });
-  
+export const authenticate = authSlice.actions.login;
+export const deauthenticate = authSlice.actions.logout;
