5be972fab2e78611bfdd12b488f6489b132e5b2d
Adam Ciecko <adam.ciecko@hotmail.com>
2023-04-12 23:29:40 +0200

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import { Dashboard } from "./views/Dashboard/Dashboard";
 import { LogIn } from "./views/Login/Login";
 import { Register } from "./views/Register/Register";
 import { Layout } from "./components";
+import { ThemeProvider } from "@mui/material";
+import {theme} from "./themes/defaultTheme";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Route path="/" exact component={LogIn} />
-        <Route path="/register" component={Register} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Route path="/" exact component={LogIn} />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
