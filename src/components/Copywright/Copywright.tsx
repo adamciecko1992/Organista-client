381f3edaac070f8a17273257d0a73bf51498cc10
jakub-malec826 <jakub.malec826@gmail.com>
2023-04-14 12:36:58 +0200

    all copywright is now a link

--- src/components/Copywright/Copywright.tsx
+++ src/components/Copywright/Copywright.tsx
@@ -1,18 +1,16 @@
 import { Link, Typography } from "@mui/material";
 
 export function Copyright(props: any) {
-    return (
-      <Typography
-        variant="body2"
-        color="text.secondary"
-        align="center"
-        {...props}
-      >
-        {"Copyright © "}
-        <Link color="inherit" href="https://mui.com/">
-         Organista 
-        </Link>
-        {new Date().getFullYear()}
-      </Typography>
-    );
-  }
+	return (
+		<Typography
+			variant="body2"
+			color="text.secondary"
+			align="center"
+			{...props}
+		>
+			<Link color="inherit" href="https://mui.com/">
+				{"Copyright © "}Organista {new Date().getFullYear()}
+			</Link>
+		</Typography>
+	);
+}
